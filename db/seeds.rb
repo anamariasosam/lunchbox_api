0e15a41f262e2280c131f99425fd62ae4dfbbefe
Ana María <anamariasosam@gmail.com>
2018-05-05 12:54:10 -0500

    create restaurants seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create([
+  {
+    name: "Señor Gourmet",
+    imageUrl: "http://lorempixel.com/400/200",
+    minPrice: 8000,
+    maxPrice: 20000,
+    location: "Bloque 4"
+   },
+   {
+    name: "Donde Tavo",
+    imageUrl: "http://lorempixel.com/400/200",
+    minPrice: 5000,
+    maxPrice: 20000,
+    location: "Bloque 4"
+   },
+   {
+    name: "Mandarina",
+    imageUrl: "http://lorempixel.com/400/200",
+    minPrice: 8000,
+    maxPrice: 20000,
+    location: "Bloque 12"
+   }
+])
